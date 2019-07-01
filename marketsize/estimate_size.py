cb27a3be4fa9af7b83a3ff21287f9eba52600d13
Casey Woolfolk <ccwoolfolk@gmail.com>
2019-06-30 19:46:13 -0500

    Round less aggressively

--- marketsize/estimate_size.py
+++ marketsize/estimate_size.py
@@ -15,6 +15,6 @@ market_size = total_market_size(
 assumed_market = market_size * config. HYPOTHETICAL_PROPORTION_OF_MARKET
 n_devs = int(round(assumed_market / config.COST_PER_DEV_FULLY_LOADED, 0))
 
-print(f'Total market size ($, millions): {round(market_size / 1_000_000, 0)}')
-print(f'Assumed addressable ($, millions): {round(assumed_market / 1_000_000, 0)}')
+print(f'Total market size ($, millions): {round(market_size / 1_000_000, 1)}')
+print(f'Assumed addressable ($, millions): {round(assumed_market / 1_000_000, 1)}')
 print(f'Assumed addressable (n devs): {n_devs}')
